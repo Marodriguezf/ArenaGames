c0ffef131d5bac28decee5cbfdee573b5994d42f
Marodriguezf <alejandra.franco1290@gmail.com>
2023-10-11 15:21:34 -0500

    commit

--- crud_php/index.php
+++ crud_php/index.php
@@ -1,25 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro de Jugadores</title>
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Link Css -->
+    <link rel="stylesheet" href="./Estilos/estilosCrud.css">
     <!-- Iconos -->
     <script src="https://kit.fontawesome.com/1c7ea47b2b.js" crossorigin="anonymous"></script>
 </head>
+
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Arena Games</a>
-            <img src="./" alt="Logo" width="66" height="80" class="d-inline-block align-text-top">
-      </a>
+            <img src="./Imagenes/Logo Arena 2.png" alt="Logo" width="66" height="80" class="d-inline-block align-text-top">
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../index.html">Inicio</a>
+                    </li>
                     <li class="nav-item">
                         <a class="nav-link" href="index.php">Registro de Jugadores</a>
                     </li>
@@ -50,10 +56,10 @@
         include "controlador/registro_persona.php";
     }
     ?>
-    <div class="container-fluid row">
+    <div class="form">
         <?php if ($mostrarFormulario) { ?>
-            <form class="col-4 p-3 m-auto" method="POST">
-                <h3 class="text-center text-secondary">Registrar Jugador</h3>
+            <form class="content col-4 p-3 m-auto" method="POST">
+            <h1 id="title">Registrate</h1>
                 <div class="mb-3">
                     <label for="nombre" class="form-label">Nombre</label>
                     <input type="text" class="form-control" name="nombre">
@@ -74,11 +80,46 @@
                     <label for="correo" class="form-label">Correo</label>
                     <input type="email" class="form-control" name="correo">
                 </div>
+
+                <div class="btn-field"> 
                 <button type="submit" class="btn btn-primary" name="btnregistrar" value="ok">Registrar</button>
+                </div>
             </form>
         <?php } ?>
     </div>
+
+        <!-- footer -->
+        <footer class="pie-pagina">
+      <div class="grupo-1">
+        <div class="box">
+          <figure>
+            <a href="#"></a>
+            <img src="./imagenes/Logo Arena 2.png" alt="Logo arena">
+          </figure>
+        </div>
+
+        <div class="box">
+          <h2>Sobre Nosotros</h2>
+          <p>Somos una plataforma que organiza torneos y eventos relacionados con el mundo gamer</p>
+        </div>
+
+        <div class="box">
+          <h2> Siguenos</h2>
+          <div class="red-social">
+            <a href="#" class="fa fa-facebook"></a>
+            <a href="#" class="fa fa-instagram"></a>
+            <a href="#" class="fa fa-whatsapp"></a>
+          </div>
+
+        </div>
+      </div>
+      <div class="grupo-2">
+        <small>&copy;2023 <b>Arena Gaming Pro</b> Todos los derechos reservados</small>
+      
+
+    </footer>
     <!-- JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
+
 </html>
--- crud_php/jugadores.php
+++ crud_php/jugadores.php
@@ -12,20 +12,52 @@ if (isset($_GET['eliminar']) && is_numeric($_GET['eliminar'])) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jugadores</title>
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Link Css -->
+    <link rel="stylesheet" href="./Estilos/estilosCrud.css">
     <!-- Iconos -->
     <script src="https://kit.fontawesome.com/1c7ea47b2b.js" crossorigin="anonymous"></script>
 </head>
+
 <body>
-    
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <img src="./Imagenes/Logo Arena 2.png" alt="Logo" width="66" height="80" class="d-inline-block align-text-top">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../index.html">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Registro de Jugadores</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="jugadores.php">Jugadores</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Registrar Torneo</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Torneos</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
     <script>
-        function eliminar(){
-            var respuesta=confirm("Estas seguro que deseas eliminar");
+        function eliminar() {
+            var respuesta = confirm("Estas seguro que deseas eliminar");
             return respuesta
 
         }
@@ -73,7 +105,39 @@ if (isset($_GET['eliminar']) && is_numeric($_GET['eliminar'])) {
             <p>No hay datos disponibles para mostrar en la tabla.</p>
         <?php } ?>
     </div>
+
+    <!-- footer -->
+    <footer class="pie-pagina">
+        <div class="grupo-1">
+            <div class="box">
+                <figure>
+                    <a href="#"></a>
+                    <img src="./imagenes/Logo Arena 2.png" alt="Logo arena">
+                </figure>
+            </div>
+
+            <div class="box">
+                <h2>Sobre Nosotros</h2>
+                <p>Somos una plataforma que organiza torneos y eventos relacionados con el mundo gamer</p>
+            </div>
+
+            <div class="box">
+                <h2> Siguenos</h2>
+                <div class="red-social">
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-instagram"></a>
+                    <a href="#" class="fa fa-whatsapp"></a>
+                </div>
+
+            </div>
+        </div>
+        <div class="grupo-2">
+            <small>&copy;2023 <b>Arena Gaming Pro</b> Todos los derechos reservados</small>
+
+
+    </footer>
     <!-- JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
+
 </html>
